59db75c47ea6f64559db6267ee00514a07417c51
trixky <mathisbois.dev@gmail.com>
2020-05-03 18:15:40 +0200

    app.js update

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const fs = require('fs')
 const puppeteer = require("puppeteer")
 
-const ms = 500
-const width = 1200
-const height = 800
-const end_delay = 10000
-
 const read_config = () => {
 	try {
 		const dataBuffer = fs.readFileSync('config.json')
@@ -27,33 +22,33 @@ const sleep = (ms) => {
 };
 
 const main = async () => {
-	const config = read_config();															// load config.json file
+	const config = read_config(); // load config.json file
 	
-	const browser = await puppeteer.launch({												// set the browser
+	const browser = await puppeteer.launch({ // set the browser
 		headless: false,
 		args: [`--window-size=${config.screen.width},${config.screen.height}`]
 	});
-	const page = await browser.newPage();													// set the page
+	const page = await browser.newPage(); // set the page
 	await page.setViewport({ width: config.screen.width, height: config.screen.height });
 	
-	await page.goto("https://typing-speed-test.aoeu.eu/");									// go to ' typing-speed-test.aoeu.eu '
+	await page.goto("https://typing-speed-test.aoeu.eu/"); // go to ' typing-speed-test.aoeu.eu '
 	
-	const cookie_button = await page.$('.cookies_ok');										// accept cookies
+	const cookie_button = await page.$('.cookies_ok'); // accept cookies
 	if (cookie_button) await cookie_button.click();
 
-	await page.evaluate((y_scroll) => window.scrollBy(0, y_scroll), config.scroll.first)	// scroll
+	await page.evaluate((y_scroll) => window.scrollBy(0, y_scroll), config.scroll.first) // scroll
 	
-	await page.focus('#input');																// focus on the input
+	await page.focus('#input'); // focus on the input
 	
-	while (await page.$('#result') == null) {												// work until results are displayed
-		let currentword_value = await page.$eval('#currentword', el => el.innerHTML);		// get the current word
-		await page.keyboard.type(currentword_value + ' ', { delay: config.delay.cpm });		// write the current word and jump to the next
+	while (await page.$('#result') == null) { // work until results are displayed
+		let currentword_value = await page.$eval('#currentword', el => el.innerHTML); // get the current word
+		await page.keyboard.type(currentword_value + ' ', { delay: config.delay.cpm }); // write the current word and jump to the next
 	}
 	
-	await page.evaluate((y_scroll) => window.scrollBy(0, y_scroll), config.scroll.second)	// scroll
+	await page.evaluate((y_scroll) => window.scrollBy(0, y_scroll), config.scroll.second) // scroll
 	
-	await sleep(config.delay.end_break);													// wait
-	await browser.close();																	// close browser
+	await sleep(config.delay.end_break); // wait
+	await browser.close(); // close browser
 }
 
 main();
